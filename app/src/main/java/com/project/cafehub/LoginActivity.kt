702c288c68366fe94ce6d5e499c207d0091cddf7
Enes Şahin <enes.sahin@ceng.deu.edu.tr>
2023-03-02 01:16:39 +0300

    Gradle dosyaları düzenlendi, json dosyası ve login sayfası eklendi.

--- app/src/main/java/com/project/cafehub/LoginActivity.kt
+++ app/src/main/java/com/project/cafehub/LoginActivity.kt
@@ -3,10 +3,9 @@ package com.project.cafehub
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val x = 0
+        setContentView(R.layout.activity_login)
     }
 }
