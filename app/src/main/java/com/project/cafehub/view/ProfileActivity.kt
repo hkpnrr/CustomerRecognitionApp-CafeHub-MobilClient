2e80cabbc2d24c6a5b1223f12efc3e4fa07d22d6
Halil Akpınar <74452544+hkpnrr@users.noreply.github.com>
2023-03-23 16:28:50 +0300

    profil sayfasına logout eklendi

--- app/src/main/java/com/project/cafehub/view/ProfileActivity.kt
+++ app/src/main/java/com/project/cafehub/view/ProfileActivity.kt
@@ -4,17 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.project.cafehub.R
 import com.project.cafehub.databinding.ActivityHomePageBinding
 import com.project.cafehub.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth=Firebase.auth
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
@@ -39,5 +45,12 @@ class ProfileActivity : AppCompatActivity() {
     fun redirectToAppSettings(view:View){
         val intent = Intent(this@ProfileActivity,ProfileActivity::class.java)
         startActivity(intent)
+    }
+
+    fun logout(view: View){
+        auth.signOut()
+        val intent = Intent(this@ProfileActivity,LoginActivity::class.java);
+        startActivity(intent)
+        finish()
     }
 }
