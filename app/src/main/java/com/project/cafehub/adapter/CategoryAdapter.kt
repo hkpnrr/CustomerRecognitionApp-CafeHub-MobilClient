96cec7b2292964940f8982568cdf0db1b38f8321
Enes Şahin <enes.sahin@ceng.deu.edu.tr>
2023-04-30 18:42:14 +0300

    Cafe menü fragment rv kayma ve yanlış product hataları çözüldü. Category modeli oluşturuldu

--- app/src/main/java/com/project/cafehub/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/project/cafehub/adapter/CategoryAdapter.kt
@@ -5,8 +5,9 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.project.cafehub.databinding.CategoryItemBinding
+import com.project.cafehub.model.Category
 
-class CategoryAdapter(val categoryList: ArrayList<String>, val categoryClickInterface: CategoryOnClickInterface)
+class CategoryAdapter(val categoryList: ArrayList<Category>, val categoryClickInterface: CategoryOnClickInterface)
     : RecyclerView.Adapter<CategoryAdapter.CategoryHolder>() {
 
     class CategoryHolder(val binding: CategoryItemBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -19,7 +20,7 @@ class CategoryAdapter(val categoryList: ArrayList<String>, val categoryClickInte
     }
 
     override fun onBindViewHolder(holder: CategoryHolder, position: Int) {
-        holder.binding.btnItemCategory.text = categoryList.get(position)
+        holder.binding.btnItemCategory.text = categoryList.get(position).name
         holder.binding.btnItemCategory.setOnClickListener {
             categoryClickInterface.onClickCategory(holder.binding.btnItemCategory)
         }
---
+++ app/src/main/java/com/project/cafehub/model/Category.kt
@@ -0,0 +1,7 @@
+package com.project.cafehub.model
+
+data class Category (
+    var id: String? = null,
+    var name: String? = null,
+    var order: Long? = null
+)
