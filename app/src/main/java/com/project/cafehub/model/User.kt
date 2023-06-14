3c2659c0e24d794fb84a633305ce3b77dbde0d5b
Enes Şahin <enes.sahin@ceng.deu.edu.tr>
2023-06-14 19:00:13 +0300

    Spotify Sayfasına en çok dinlenilen şarkıları ekleme özelliği eklendi

--- app/src/main/java/com/project/cafehub/model/User.kt
+++ app/src/main/java/com/project/cafehub/model/User.kt
@@ -1,14 +1,13 @@
 package com.project.cafehub.model
 import java.io.Serializable
-import java.util.Date
 
 data class User(
     var id: String? = null,
-    var name:String? = null,
-    var surname:String? = null,
-    var email:String? = null,
-    var birthdate:String? = null,
-    var photoUrl:String?=null,
-    var isActive:Boolean?=null,
-    var activeCafeId:String?=null) : Serializable {
+    var name: String? = null,
+    var surname: String? = null,
+    var email: String? = null,
+    var birthdate: String? = null,
+    var photoUrl: String? = null,
+    var isActive: Boolean? = null,
+    var activeCafeId: String? = null) : Serializable {
 }
--- app/src/main/java/com/project/cafehub/view/currentCafe/SpotifyActivity.kt
+++ app/src/main/java/com/project/cafehub/view/currentCafe/SpotifyActivity.kt
@@ -4,15 +4,22 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.project.cafehub.R
+import com.project.cafehub.adapter.TopTrackAdapter
 import com.project.cafehub.databinding.ActivitySpotifyBinding
-import com.project.cafehub.model.AccessTokenResponse
-import com.project.cafehub.model.Cafe
-import com.project.cafehub.model.Product
+import com.project.cafehub.model.*
+import com.project.cafehub.model.CurrentUser.user
+import com.project.cafehub.view.chat.ChatActivity
 import com.spotify.sdk.android.auth.AuthorizationClient
 import com.spotify.sdk.android.auth.AuthorizationRequest
 import com.spotify.sdk.android.auth.AuthorizationResponse
@@ -22,32 +29,29 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class SpotifyActivity : AppCompatActivity() {
+class SpotifyActivity : AppCompatActivity(), TopTrackAdapter.SongItemClickListener {
 
     private lateinit var binding : ActivitySpotifyBinding
     private lateinit var db: FirebaseFirestore
+    private lateinit var currentCafe: Cafe
+    private lateinit var topTrackList: ArrayList<TopTrack>
+    private lateinit var topTrackAdapter: TopTrackAdapter
 
     private val REDIRECT_URI = "com.project.cafehub://callback"
-
     private val CLIENT_ID = "66ece2d4f8484e30af4fbce390bae1c9"
     private val CLIENT_SECRET = "491497f6b5774c2ab3ac4b746dead0cb"
     private val BASE_URL_AUTH = "https://accounts.spotify.com"
     private val BASE_URL_API = "https://api.spotify.com"
-
     private val REQUEST_CODE = 1337
-
-
-    private lateinit var authorizationCode: String
 
     private lateinit var authService: SpotifyAuthService
     private lateinit var apiService: SpotifyApiService
 
-    private lateinit var userAccessToken: String
+    private lateinit var authorizationCode: String
+    private var userAccessToken: String? = null
     private lateinit var cafeAccessToken: String
-    private lateinit var refreshToken: String
-    private var tokenExpirationTime: Long = 0
-
-    private lateinit var currentCafe: Cafe
+    private var refreshToken: String? = null
+    private var tokenExpirationTime: Long? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,13 +61,26 @@ class SpotifyActivity : AppCompatActivity() {
 
         db = Firebase.firestore
         currentCafe = (intent.getSerializableExtra("currentCafe") as Cafe?)!!
+        userAccessToken = (intent.getSerializableExtra("userAccessToken") as String?)
+        refreshToken = (intent.getSerializableExtra("refreshToken") as String?)
+        (intent.getSerializableExtra("tokenExpirationTime") as Long?)?.let{
+            tokenExpirationTime = it
+        }
+        topTrackList = ArrayList<TopTrack>()
 
         initToolbar()
 
         setupServices()
         getCafeAccessToken()
-        handleLoginButtonClick()
+        if (userAccessToken.isNullOrEmpty() || refreshToken.isNullOrEmpty() || tokenExpirationTime==null || tokenExpirationTime==0.toLong()) {
+            handleLoginButtonClick()
+        }
         handleSendSongButtonClick()
+        getTopTracks()
+        binding.rvUserTopTracks.layoutManager = LinearLayoutManager(this)
+        topTrackAdapter = TopTrackAdapter(topTrackList, this)
+        binding.rvUserTopTracks.adapter = topTrackAdapter
+
     }
 
     private fun initToolbar(){
@@ -86,6 +103,17 @@ class SpotifyActivity : AppCompatActivity() {
             }
     }
 
+    private fun saveUserAccessTokenInfo() {
+        db.collection("User").document(user.id!!)
+            .update(mapOf(
+                "userAccessToken" to userAccessToken,
+                "refreshToken" to refreshToken,
+                "tokenExpirationTime" to tokenExpirationTime
+            ))
+            .addOnFailureListener{
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     private fun setupServices() {
         val retrofit_auth = Retrofit.Builder()
@@ -103,7 +131,7 @@ class SpotifyActivity : AppCompatActivity() {
 
     private fun handleLoginButtonClick() {
         val authRequest = AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.CODE, REDIRECT_URI)
-            .setScopes(arrayOf("user-modify-playback-state"))
+            .setScopes(arrayOf("user-modify-playback-state, user-top-read"))
             .build()
 
         //AuthorizationClient.openLoginActivity(activity, REQUEST_CODE, authRequest)
@@ -144,7 +172,7 @@ class SpotifyActivity : AppCompatActivity() {
                 AuthorizationResponse.Type.TOKEN -> {
                     // Handle successful response
                     cafeAccessToken = response.accessToken
-                    //addToPlaybackQueue("spotify:track:4iV5W9uYEdYUVa79Axb7Rh")
+                    // Call Api directly
                 }
                 AuthorizationResponse.Type.CODE -> {
                     // Authorization code obtained successfully
@@ -181,12 +209,13 @@ class SpotifyActivity : AppCompatActivity() {
 
                     val accessTokenResponse = response.body()
                     if (accessTokenResponse != null) {
-                        userAccessToken = accessTokenResponse.accessToken
-                        refreshToken = accessTokenResponse.refreshToken.toString()
-                        tokenExpirationTime = System.currentTimeMillis() + (accessTokenResponse.expiresIn * 1000)
                         // Store the access token, refresh token, and expiration time securely for future API requests
-                        // addToPlaybackQueue("spotify:track:4UySkSnMBKf1PS32agnwxp")
-                    }//https://open.spotify.com/track/4UySkSnMBKf1PS32agnwxp?si=731fdbb5eaa449d8
+                        // accessToken = accessTokenResponse.accessToken
+                        userAccessToken = accessTokenResponse.accessToken
+                        refreshToken = accessTokenResponse.refreshToken
+                        tokenExpirationTime = System.currentTimeMillis() + (accessTokenResponse.expiresIn * 1000)
+                        saveUserAccessTokenInfo()
+                    }
                 } else {
                     // Handle authorization error
                     // Handle the error accordingly
@@ -199,7 +228,7 @@ class SpotifyActivity : AppCompatActivity() {
     }
 
     fun handleSendSongButtonClick(){
-        binding.sendTheSongButton.setOnClickListener {                                           // .token dene for activity
+        binding.sendTheSongButton.setOnClickListener {
             val songUrl = binding.editTextSongUrl.text.toString()
             val regex = Regex("""track/([a-zA-Z0-9]+)""")
             val matchResult = regex.find(songUrl)
@@ -214,9 +243,9 @@ class SpotifyActivity : AppCompatActivity() {
     }
 
     private fun addToPlaybackQueue(songUri: String, activity: Activity) {
-        if (System.currentTimeMillis() >= tokenExpirationTime) {
+        if (System.currentTimeMillis() >= tokenExpirationTime!!) {
             // Access token has expired, refresh the token
-            refreshAccessToken()
+             refreshAccessToken()
         } else {
             val token = "BQBrUcp1fzKa91Ekj7SWKBrrIzwBij66voiCcQtkYSvE7ajNzadsRfrr9z9uFvbS3Fie5CNpnTTCprJx-b9-2WcrjhxryfTYYNNTV2aANC9f98zz2R24L0MdLlEZq-CDJu3mjjXCt4jru3jv1G8lX1iDqfhjYELwKl48Qm1wocdNhlKLC4SGcOrlbgEo0hTlXyeS1cntNdD2x1bwojhS1p-C18H5F3wwj70Npw6gAkEp"
             val authHeader = "Bearer $userAccessToken"
@@ -246,7 +275,7 @@ class SpotifyActivity : AppCompatActivity() {
     private fun refreshAccessToken() {
         val requestBody = HashMap<String, String>()
         requestBody["grant_type"] = "refresh_token"
-        requestBody["refresh_token"] = refreshToken
+        requestBody["refresh_token"] = refreshToken!!
         requestBody["client_id"] = CLIENT_ID
         requestBody["client_secret"] = CLIENT_SECRET
 
@@ -260,19 +289,101 @@ class SpotifyActivity : AppCompatActivity() {
                     val accessTokenResponse = response.body()
                     if (accessTokenResponse != null) {
                         userAccessToken = accessTokenResponse.accessToken
+                        refreshToken = accessTokenResponse.refreshToken
                         tokenExpirationTime = System.currentTimeMillis() + (accessTokenResponse.expiresIn * 1000)
-                        // Update the access token and expiration time securely
-                        //addToPlaybackQueue("spotify:track:4iV5W9uYEdYUVa79Axb7Rh")
+                        saveUserAccessTokenInfo()
                     }
                 } else {
-                    // Handle token refresh error
-                    // Handle the error accordingly
+                    // Handle token error
                 }
             }
-
             override fun onFailure(call: Call<AccessTokenResponse>, t: Throwable) {
                 // Handle network or request failure
             }
         })
+    }
+
+    private fun getTopTracks() {
+        if (System.currentTimeMillis() >= tokenExpirationTime!!) {
+            // Access token has expired, refresh the token
+            refreshAccessToken()
+        } else {
+            val authHeader = "Bearer $userAccessToken"
+            val timeRange = "medium_term"
+            val limit = 10
+
+            val getTopTracksCall = apiService.getTopTracks(authHeader, timeRange, limit)
+            getTopTracksCall.enqueue(object : Callback<TopTracksResponse> {
+                //@SuppressLint("NotifyDataSetChanged")
+                override fun onResponse(call: Call<TopTracksResponse>, response: Response<TopTracksResponse>) {
+                    if (response.isSuccessful) {
+                        val topTracksResponse = response.body()
+                        val topTracks = topTracksResponse?.items ?: emptyList()
+
+                        /*topTracksResponse?.items?.map { item ->
+                            val artists = item.artists//.map { it.name }
+                            val imageUrl = item.album//.images.firstOrNull()?.imageUrl ?: ""
+                            TopTrackItem(item.name, artists, imageUrl)
+                        }*/
+
+                        topTracks?.forEach { track ->
+                            val trackUri = track.uri
+                            val trackName = track.name
+                            val artists = track.artists//.map { it.name }
+                            val imageUrl = track.album.images.firstOrNull()?.imageUrl ?: ""
+                            //val trackUrl = track.externalUrls["spotify"] ?: ""
+                            val topTrack = TopTrack(trackUri, trackName, artists, track.album, imageUrl)
+
+                            topTrackList.add(topTrack)
+                        }
+                        topTrackAdapter.notifyDataSetChanged()
+                    } else {
+                        println("Failed to retrieve top tracks")
+                    }
+                }
+                override fun onFailure(call: Call<TopTracksResponse>, t: Throwable) {
+                    // Handle failure
+                    println("Failed to retrieve top tracks")
+                }
+            })
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here.
+        val id = item.itemId
+
+        if(id== R.id.chat){
+            val intent = Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun sendSongInfoClicked(view: View) {
+        val builder = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val dialogView = inflater.inflate(R.layout.custom_dialog_layout_send_song_info, null)
+
+        builder.setPositiveButton("Tamam") { dialog, which ->
+            dialog.cancel()
+        }
+        builder.setView(dialogView)
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onSendSongClicked(trackUri: String) {
+        addToPlaybackQueue(trackUri, this)
+        //https://open.spotify.com/track/4wLpDd099QT7TmNpMkFIvT?si=e89b38e8a26646c7
+        //spotify:track:4wLpDd099QT7TmNpMkFIvT
+        //spotify:track:4wLpDd099QT7TmNpMkFIvT
     }
 }
